41412b9430bd5b9c6ba193c3e9cce69240e0e479
seho0808 <seho0808@vt.edu>
2023-11-14 21:31:21 +0900

    config: removed gatsby path prefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  pathPrefix: "/portfolio",
+  pathPrefix: "",
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
