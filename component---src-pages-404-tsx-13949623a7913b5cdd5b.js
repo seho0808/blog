696775f51998de290416c780dc52a3bab0066280
Seho Lee <seho0808@vt.edu>
2025-09-14 16:17:59 +0900

    Updates

---
+++ component---src-pages-404-tsx-13949623a7913b5cdd5b.js
@@ -0,0 +1,20 @@
+"use strict";(self.webpackChunkgatsby_starter_minimal_ts=self.webpackChunkgatsby_starter_minimal_ts||[]).push([[453],{731:function(e,t,n){n.r(t),n.d(t,{Head:function(){return r}});var l=n(6540),a=n(7627);const i=a.A.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: #ccc;
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+`,c=a.A.a`
+  color: #a6e22e;
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;t.default=()=>l.createElement(i,null,l.createElement("h1",null,"페이지를 찾을 수 없었습니다."),l.createElement(c,{href:"/"},l.createElement("div",null,"메인으로")));const r=()=>l.createElement("title",null,"Not found")}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-13949623a7913b5cdd5b.js.map
